b170fc89572022e5a00432a863185dce7ce1bbda
mrstruijk <mrstruijk@gmail.com>
2025-02-12 14:23:26 +0100

    Update CreateDemoLogs.cs

--- Samples~/Samples/CreateDemoLogs.cs
+++ Samples~/Samples/CreateDemoLogs.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 public class CreateDemoLogs : MonoBehaviour
 {
     public string IdentifyerText;
-    
-    
+
+
     private void Start()
     {
         Debug.LogError("This is an error log");
-        
+
         Debug.LogFormat("The material gets switched on the gameObject {0}", IdentifyerText);
     }
 
 
     private void Update()
     {
-        Debug.LogFormat(this, "This is a log with a context of the name of the GameObject where it came from: {0}", this.gameObject.name);
+        Debug.LogFormat(this, "This is a log with a context of the name of the GameObject where it came from: {0}", gameObject.name);
     }
 
 
